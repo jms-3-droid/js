bc92eb4ea894c93431fadde36ca6e09ee630d778
JMS <180058073+jms-3-droid@users.noreply.github.com>
2025-06-03 14:45:10 -0700

    js lesson one

---
+++ javascript.js
@@ -0,0 +1,13 @@
+let name = "John";
+let surname = "Doe";
+let age = 11;
+
+age = 54
+
+const pi = 3.14
+
+console.log(name);
+console.log(surname);
+console.log(age);
+console.log(pi);
+console.log((3 + 2) - 76 * (1 + 1));
