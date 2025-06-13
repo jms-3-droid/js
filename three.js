85818a92f2d96e31d3ef7d50f63e17c711e28a0f
JMS <180058073+jms-3-droid@users.noreply.github.com>
2025-06-13 11:46:50 -0700

    code dom exercises

---
+++ three.js
@@ -0,0 +1,28 @@
+const container = document.querySelector("#container");
+
+const content = document.createElement("div");
+content.classList.add("content");
+content.textContent = "This is the glorious text-content!";
+
+container.appendChild(content);
+
+const red = document.createElement("p");
+red.style.color = "red";
+red.textContent = "Hey, I'm red!";
+container.appendChild(red);
+
+const blue = document.createElement("h3");
+blue.style.color = "blue";
+blue.textContent = "I'm a blue h3!";
+container.appendChild(blue);
+
+const newdiv = document.createElement("div");
+newdiv.style.border = "black";
+newdiv.style.backgroundColor = "pink";
+const indiv = document.createElement("h1");
+indiv.textContent = "I'm in a div";
+newdiv.appendChild(indiv);
+const newp = document.createElement("p");
+newp.textContent = "ME TOO!";
+newdiv.appendChild(newp);
+container.appendChild(newdiv);
