c9c3452bfa25dbea351e0ac34f1b31ca15529831
JMS <180058073+jms-3-droid@users.noreply.github.com>
2025-06-13 12:08:43 -0700

    code js exercise

--- three.js
+++ three.js
@@ -26,3 +26,11 @@ const newp = document.createElement("p");
 newp.textContent = "ME TOO!";
 newdiv.appendChild(newp);
 container.appendChild(newdiv);
+
+const btn = document.querySelector("#btn");
+btn.onclick = () => alert("Hello World");
+
+const btn2 = document.querySelector("#btn2");
+btn2.addEventListener("click", () => {
+  alert("Hello World");
+});
